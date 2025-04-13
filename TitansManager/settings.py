3f6cfcf437dc287333bd054e9d282523ab8fc54a
Femi Oladele <profemzy@gmail.com>
2025-04-12 18:22:16 -0700

    Use inmemory db for test in CI

--- TitansManager/settings.py
+++ TitansManager/settings.py
@@ -179,16 +179,26 @@ WSGI_APPLICATION = "TitansManager.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("DATABASE_NAME", "tmsdb"),
-        "USER": os.getenv("DATABASE_USERNAME", "dbuser"),
-        "PASSWORD": os.getenv("DATABASE_PASSWORD", "dbpassword"),
-        "HOST": os.getenv("DATABASE_HOST", "127.0.0.1"),
-        "PORT": os.getenv("DATABASE_PORT", 5432),
+import sys
+
+if 'test' in sys.argv or os.getenv('CI') == 'true':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': ':memory:',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.getenv('DB_NAME'),
+            'USER': os.getenv('DB_USER'),
+            'PASSWORD': os.getenv('DB_PASSWORD'),
+            'HOST': os.getenv('DB_HOST'),
+            'PORT': os.getenv('DB_PORT', '5432'),
+        }
+    }
 
 AUTH_USER_MODEL = "core.User"
 
